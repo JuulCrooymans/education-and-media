f3c606c4c587a8a4273d9cc5d2e3414a3e221b9a
Juul Crooymans <juul.crooymans@gmail.com>
2020-05-17 16:06:36 +0200

    added 404 page

---
+++ client/src/components/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h1>404: Page not found</h1>
+        <br>
+        <router-link to="/">Home</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '404'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

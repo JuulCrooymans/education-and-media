d8481dacd294a6d104f7276e64dda83278babe6e
Juul Crooymans <juul.crooymans@gmail.com>
2020-05-17 16:38:17 +0200

    removed jwt import

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
-const jwt = require("express-jwt"); 
-const jwksRsa = require("jwks-rsa");
 
 const app = express();
 

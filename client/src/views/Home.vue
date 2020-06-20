6af238a0597f8bf8eb16839376be3ebab9e1ba11
Juul Crooymans <juul.crooymans@gmail.com>
2020-06-20 23:03:45 +0200

    changed homepage

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="page page--padding">
-        <PostComponent />
+        <div class="home-title">
+            <h2>Welkom</h2>
+            <h2 class="home-title__user">{{ $auth.user.nickname }}</h2>
+        </div>
+        <div class="content-placeholder">
+            <p>Content placeholder</p>
+        </div>
     </div>
 </template>
 
@@ -42,4 +48,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.home-title {
+    display: flex;
+    margin-bottom: 60px;
+
+    &__user {
+        font-weight: 400;
+        margin-left: 30px;
+    }
+}
+
+.content-placeholder {
+    height: 500px;
+    width: 100%;
+    background: $xlight;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    p {
+        color: $dark;
+        opacity: .3;
+    }
+}
 </style>
